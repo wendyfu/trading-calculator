af931b7fe7ab2b9f960f17dda1d767ca6de9d62a
wendyfu <fpt.wendy@gmail.com>
2024-08-25 18:18:43 +0900

    init state structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,98 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { ChangeEvent, useActionState, useState } from 'react'
+
+interface CalculatorState {
+  openPrice: number
+  stopLossPrice: number
+  pips: number
+  capital: number
+  riskPercentage: number
+  [key: string]: number // Add index signature
+}
 
 function App() {
+  const [lotSize, setLotSize] = useState(0)
+  const [riskAmount, setRiskAmount] = useState(0)
+
+  const [state, action] = useActionState<CalculatorState>(
+    (state: CalculatorState) => {
+      return state
+    },
+    {
+      openPrice: 0,
+      stopLossPrice: 0,
+      pips: 0,
+      capital: 2000,
+      riskPercentage: 2,
+    }
+  )
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    state[name] = parseFloat(value) || 0
+    action()
+  }
+
   return (
     <Card className="w-[400px]">
       <CardHeader>
         <CardTitle>Lot Size Calculator</CardTitle>
         <CardDescription>Tailored for Orbi Trade</CardDescription>
       </CardHeader>
-      <CardContent className="">
+      <CardContent>
         <div className="grid grid-cols-2 gap-4 mb-2 pb-2">
           <div className="grid items-center gap-2">
             <Label htmlFor="open-price">Open price</Label>
-            <Input id="open-price" placeholder="Entry price" />
+            <Input
+              id="open-price"
+              placeholder="Entry price"
+              onChange={handleChange}
+              name="openPrice"
+              value={state.openPrice}
+            />
           </div>
           <div className="grid items-center gap-2">
             <Label htmlFor="stop-loss-price">Stop loss price</Label>
-            <Input id="stop-loss-price" placeholder="Stop loss price" />
+            <Input
+              id="stop-loss-price"
+              placeholder="Stop loss price"
+              onChange={handleChange}
+              name="stopLossPrice"
+              value={state.stopLossPrice}
+            />
           </div>
         </div>
         <div className="grid items-center gap-2 mb-2 pb-2">
           <Label htmlFor="pips">Pips</Label>
-          <Input id="pips" placeholder="Pips" />
+          <Input
+            id="pips"
+            placeholder="Pips"
+            onChange={handleChange}
+            name="pips"
+            value={state.pips}
+          />
         </div>
         <div className="grid grid-cols-3 gap-4">
           <div className="grid items-center gap-2 col-span-2">
             <Label htmlFor="capital">Capital</Label>
-            <Input id="capital" placeholder="Capital amount" />
+            <Input
+              id="capital"
+              placeholder="Capital amount"
+              onChange={handleChange}
+              name="capital"
+              value={state.capital}
+            />
           </div>
           <div className="grid items-center gap-2">
             <Label htmlFor="risk-percentage">Risk (%)</Label>
-            <Input id="risk-percentage" placeholder="Risk" />
+            <Input
+              id="risk-percentage"
+              placeholder="Risk"
+              onChange={handleChange}
+              name="riskPercentage"
+              value={state.riskPercentage}
+            />
           </div>
         </div>
       </CardContent>
@@ -46,7 +107,7 @@ function App() {
         <div>
           <p className="text-sm text-muted-foreground">Max trade size</p>
           <h3 className="font-semibold tracking-tight flex items-baseline gap-1 text-4xl tabular-nums">
-            322
+            {lotSize || '--'}
             <span className="text-sm font-normal tracking-normal text-muted-foreground">
               lot(s)
             </span>
@@ -58,7 +119,7 @@ function App() {
             <span className="text-sm font-normal tracking-normal text-muted-foreground">
               US$
             </span>
-            40.00
+            {riskAmount || '--'}
           </h3>
         </div>
       </CardFooter>
